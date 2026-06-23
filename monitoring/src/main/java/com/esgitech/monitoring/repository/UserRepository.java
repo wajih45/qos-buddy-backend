8e3ccabeb7ca340f6e57b25407d5aaec9fb0ed42
wajih <wajih.ablellaoui@esgitech.tn>
2026-06-23 12:10:23 +0100

    Remove duplicate folder

--- monitoring/src/main/java/com/esgitech/monitoring/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.esgitech.monitoring.repository;
-
-import com.esgitech.monitoring.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
